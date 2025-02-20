22f71fca7908ba3b4d90f9750ebbab940f00491a
eltonss <eltonss@ufpa.br>
2025-02-20 01:31:35 -0300

    modified:   Util/GoogleSheetsReader.py

--- Util/GoogleSheetsReader.py
+++ Util/GoogleSheetsReader.py
@@ -1,7 +1,10 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
-
+from dotenv import load_dotenv
+import os
+# Carregar variáveis de ambiente do arquivo .env
+load_dotenv()
 # Configuração da API do Google
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 CREDENTIALS_FILE = "Keys/credentials.json"  # Baixe do Google Cloud Console
@@ -10,7 +13,7 @@ creds = Credentials.from_service_account_file(CREDENTIALS_FILE, scopes=SCOPES)
 client = gspread.authorize(creds)
 
 # Acessar a planilha de respostas do Google Forms
-SHEET_ID = "1Y6QXrAQbVxf4H4fTEPnBaExqll2xAxEZgMjx-o_h-rU"
+SHEET_ID =os.getenv("SHEET_ID") 
 sheet = client.open_by_key(SHEET_ID).sheet1
 
 # Obter todas as respostas
